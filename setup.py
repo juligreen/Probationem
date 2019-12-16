5b10ca2b69889b607ae79b807470277b40be3f5b
Julius Dehner <julius.dehner@gmail.com>
2019-12-16 15:36:40 +0100

    Update Version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name='Probationem',
     packages=['Probationem'],
-    version='1.0.4',
+    version='1.1.0',
     license='MIT',
     description='Generator fuer Ausbildungsnachweise (Fachinformatiker)',
     author='Julius Dehner',
     author_email='julius.dehner@gmail.com',
     url='https://github.com/juligreen/Probationem',
-    download_url='https://github.com/juligreen/Probationem/archive/1.0.4.tar.gz',
+    download_url='https://github.com/juligreen/Probationem/archive/1.1.0.tar.gz',
     keywords=['generator', 'certificate', 'automation'],
     install_requires=[
         'fpdf',
@@ -18,6 +18,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     entry_points={
         "console_scripts": [
